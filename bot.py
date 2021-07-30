66790c724800efe3be886cb3dbacaa0a4ec6602c
Mathieu.D <dupycournau@gmail.com>
2021-07-30 16:42:10 +0200

    add command gallery

--- bot.py
+++ bot.py
@@ -20,17 +20,33 @@ class DiscordClient(discord.Client):
         print('ready')
 
     async def on_message(self, message):
-        splitted = message.split()
-        for word in splitted:
-            if "https://twitter.com/" in word:
-                tweet_info = await get_tweet_info(word, HEADER)
-                final_data = json.loads(tweet_info)
-                if final_data['includes'][0]['type']:
-                    text = word
-                    position = text.find("twitter.com")
-                    final_string = text[:position] + "fx" + text[position:]
-                    await message.channel.send(f"{message.author.display_name} a posté ceci {final_string}")
+        if message.content.startswith('!gallery'):
+            splitted = message.content.split()
+            link = splitted[1]
+            if "https://twitter.com/" in link:
+                data = await get_tweet_info(link, HEADER)
+                try:
+                    final_data = json.loads(data)
+                    media_list = final_data['includes']['media']
+                    for media in media_list:
+                        await message.channel.send(media['url'])
+                    await message.channel.send(f"Source : <{link}>")
                     await message.delete()
+                except KeyError:
+                    await message.channel.send("Aucun media dans ce tweet")
+
+        else:
+            splitted = message.content.split()
+            for word in splitted:
+                if "https://twitter.com/" in word:
+                    tweet_info = await get_tweet_info(word, HEADER)
+                    final_data = json.loads(tweet_info)
+                    if final_data['includes']['media'][0]['type'] in {'video', 'animated_gif'}:
+                        text = word
+                        position = text.find("twitter.com")
+                        final_string = text[:position] + "fx" + text[position:]
+                        await message.channel.send(f"{message.author.display_name} a posté ceci {final_string}")
+                        await message.delete()
 
 
 client = DiscordClient()
